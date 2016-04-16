72fa088556ce52fcc2b583257e7497687ea053cb
eaofxr <eawade@xresults.com>
2016-04-16 23:12:15 +0800

    test
    test

--- js/usMap.js
+++ js/usMap.js
@@ -158,4 +158,16 @@ USMap.prototype.updateColors = function(keyVar){
                 return vis.colorScale(keyById[d.id]); 
             }
         });
+
+
+
+    d3.select("div#usa-map")
+        .append("div")
+        .classed("svg-container", true) //container class to make it responsive
+        .append("svg")
+        //responsive SVG needs these 2 attributes and no width and height attr
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 600 400")
+        //class to make it responsive
+        .classed("svg-content-responsive", true);
 }
